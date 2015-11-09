71b4d598ddaf1be6eaa77aca315bbdd748fd988e
Lucas Araujo <lucas.lra@gmail.com>
2015-11-09 16:59:02 -0200

    Saving Attachments

---
+++ OutlookAddinTests/ThisAddIn.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using Office = Microsoft.Office.Core;
+
+namespace OutlookAddinTests
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            Application.ItemSend += Application_ItemSend;
+        }
+
+        void Application_ItemSend(object Item, ref bool Cancel)
+        {
+            foreach (Outlook.Attachment attachment in ((Outlook.MailItem)Item).Attachments)
+            {
+                attachment.SaveAsFile(Path.Combine("C:\\Rep", attachment.DisplayName));
+            }
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+
+        #endregion
+    }
+}
